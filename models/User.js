b80d6e4b82124f876df00213e84e5d61d3dc7c12
hrmsystemsngl <hrmsystems.ngl@gmail.com>
2025-05-19 14:50:16 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
-  role: { type: String, enum: ['Employee', 'Agent', 'Admin'], default: 'Employee' },
-  password: String
+  role: {
+    type: String,
+    enum: ['Employee', 'Agent', 'Manager', 'Admin'],
+    default: 'Employee'
+  },
+  password: String,
+  manager: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // NEW
 });
 
 module.exports = mongoose.model('User', userSchema);
